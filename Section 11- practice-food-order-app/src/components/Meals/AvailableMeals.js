dc38e97c61da8147bf7f8c79663efa4a966d181a
Mahmoud Fouad <mfapolice@gmail.com>
2023-05-04 05:11:05 +0300

    update

--- Section 11- practice-food-order-app/src/components/Meals/AvailableMeals.js
+++ Section 11- practice-food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -13,7 +14,7 @@ const DUMMY_MEALS = [
     id: "m2",
     name: "Schnitzel",
     description: "A german specialty!",
-    price: 16.5, 
+    price: 16.5,
   },
   {
     id: "m3",
@@ -35,9 +36,12 @@ const AvailableMeals = () => {
       <Card>
         <ul>
           {DUMMY_MEALS.map((meal) => (
-            <Card  key={meal.name}>
-              <li>{meal.name}</li>
-            </Card>
+            <MealItem
+              key={meal.id}
+              name={meal.name}
+              description={meal.description}
+              price={meal.price}
+            />
           ))}
         </ul>
       </Card>
