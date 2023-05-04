abc20a46d70a50a27a24b5aa9b0920cb33cc558d
Mahmoud Fouad <mfapolice@gmail.com>
2023-05-04 03:51:14 +0300

    first commit

---
+++ Section 11- practice-food-order-app/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <h2>Let's get started!</h2>
+    </Fragment>
+  );
+}
+
+export default App;
