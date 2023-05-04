24a9d0a03231ec13b860506c87d426c33a7cb674
Mahmoud Fouad <mfapolice@gmail.com>
2023-05-04 03:54:11 +0300

    update

--- Section 11- practice-food-order-app/src/components/Layout/HeaderCartButton.js
+++ Section 11- practice-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -4,7 +4,7 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = () => {
   return (
-    <button className={classes.botton}>
+    <button className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
