576f21802d837b5965329497b9de234c8e934511
Mahmoud Fouad <mfapolice@gmail.com>
2023-05-04 06:06:11 +0300

    fix value in input

--- Section 11- practice-food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ Section 11- practice-food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -2,16 +2,15 @@ import React from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
-const MealItemForm = () => {
+const MealItemForm = props => {
   return (
     <form className={classes.form}>
       <Input
         label="Amount"
         input={{
-          id: "amount",
+          id: props.id,
           type: "number",
           min: 0,
-          value: 1,
           step: "1",
           defaultValue: "1",
           onChange: (e) => console.log(e.target.value),
