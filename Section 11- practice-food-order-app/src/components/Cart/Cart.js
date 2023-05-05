09b81c273546e33d2da46fa9dfae58ca14f8f0cb
Mahmoud Fouad <mfapolice@gmail.com>
2023-05-05 04:38:49 +0300

    creating Cart

--- Section 11- practice-food-order-app/src/components/Cart/Cart.js
+++ Section 11- practice-food-order-app/src/components/Cart/Cart.js
@@ -11,12 +11,14 @@ const Cart = () => {
   ];
   return (
     <div>
-      <div>
-        <ul className={classes["cart-items"]}>
-          {cartItems.map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-        </ul>
+      <ul className={classes["cart-items"]}>
+        {cartItems.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
       </div>
       <div className={classes.actions}>
         <button>Close</button>
